6ad11200b4836def3d92d15e3a2fdc5d3845dd17
burbulicious <89243870+burbulicious@users.noreply.github.com>
2024-04-12 16:41:47 +0300

    setup

---
+++ src/components/icons/IconClose.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps({
+  iconColour: {
+    type: String,
+    required: true
+  }
+})
+</script>
+<template>
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M6.87389 4L4 6.87389L5.46752 8.3414L9.09554 12.0306L5.46752 15.6586L4 17.065L6.87389 20L8.3414 18.5325L12.0306 14.8433L15.6586 18.5325L17.065 20L20 17.065L18.5325 15.6586L14.8433 12.0306L18.5325 8.3414L20 6.87389L17.065 4L15.6586 5.46752L12.0306 9.09554L8.3414 5.46752L6.87389 4Z"
+      :fill="iconColour"
+    />
+  </svg>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        grey: {
+          DEFAULT: '#e6e6e6',
+          900: '#191919',
+          850: '#303030',
+          800: '#5E5E5E',
+          700: '#C0C0C0'
+        },
+        yellow: {
+          DEFAULT: '#ffd000',
+          100: '#f6f2dd'
+        },
+        green: {
+          DEFAULT: '#73a46e',
+          light: '#edf7e7'
+        },
+        red: {
+          DEFAULT: '#d55b60',
+          light: '#ffdcd9'
+        },
+        blue: {
+          DEFAULT: '#409cdd',
+          light: '#e3edf3'
+        }
+      },
+      screens: {
+        xs: '420px',
+        // => @media (min-width: 420px) { ... }
+
+        sm: '640px',
+        // => @media (min-width: 640px) { ... }
+
+        md: '768px',
+        // => @media (min-width: 768px) { ... }
+
+        lg: '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        xl: '1280px',
+        // => @media (min-width: 1280px) { ... }
+
+        '2xl': '1536px'
+        // => @media (min-width: 1536px) { ... }
+      }
+    }
+  },
+  plugins: []
+}
