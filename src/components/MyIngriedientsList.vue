cb6f97741745458a929f225759e9a30c232aa7b2
burbulicious <89243870+burbulicious@users.noreply.github.com>
2024-04-20 17:44:46 +0300

    filter ingredients

--- src/components/MyIngriedientsList.vue
+++ src/components/MyIngriedientsList.vue
@@ -6,37 +6,36 @@ import IngredientListItem from '@/components/IngredientListItem.vue'
 import ButtonComponent from '@/components/ButtonComponent.vue'
 
 const mealPlanStore = useMealPlanStore()
-const ingredients = computed<string>(() => mealPlanStore.ingredients)
 const ingredientsList = computed<string[]>(() => {
-  const list = ingredients.value.split(',')
-  return list.map((item) => item.trim())
+  const list = myIngredients.value.split(',')
+  return list.map((item) => item.trim().toLowerCase())
 })
+const allIngredients = computed<string[]>(() => [
+  ...mealPlanStore.extractedIngredients.map((item) => item.name)
+])
+const myIngredients = computed<string>(() => mealPlanStore.ingredients)
 
 const newIngredient = ref<string>('')
-const itemExistsInIngredients = computed<boolean>(() =>
-  ingredientsList.value
+const inputExistsInIngredients = computed<boolean>(() => {
+  const isFoundInMyIngredients: boolean = ingredientsList.value.includes(
+    newIngredient.value.trim().toLowerCase()
+  )
+  const isFoundInAllIngredients: boolean = allIngredients.value
     .map((item) => item.trim().toLowerCase())
     .includes(newIngredient.value.trim().toLowerCase())
-)
+
+  return isFoundInMyIngredients || isFoundInAllIngredients
+})
 const isAddButtonDisabled = computed<boolean>(
-  () => newIngredient.value === '' || itemExistsInIngredients.value
+  () => newIngredient.value === '' || inputExistsInIngredients.value
 )
-// const isRemoveButtonDisabled = computed<boolean>(
-//   () => newIngredient.value === '' || !itemExistsInIngredients.value
-// )
 
 const addNewIngredient = () => {
-  if (!itemExistsInIngredients.value) {
+  if (!inputExistsInIngredients.value) {
     mealPlanStore.addIngredient(newIngredient.value)
     newIngredient.value = ''
   }
 }
-// const removeExistingIngredient = () => {
-//   if (itemExistsInIngredients.value) {
-//     mealPlanStore.removeIngredient(newIngredient.value)
-//     newIngredient.value = ''
-//   }
-// }
 </script>
 
 <template>
@@ -48,17 +47,16 @@ const addNewIngredient = () => {
       @click="addNewIngredient"
       buttonText="+ Add"
     />
-    <GenerateMealPlanBtn v-if="ingredients" buttonText="Generate new meal plan" />
+    <GenerateMealPlanBtn v-if="myIngredients" buttonText="Generate new meal plan" />
   </div>
-  <div v-if="!!ingredients">
-    <ul>
+  <div v-if="myIngredients">
+    <ul v-for="(item, index) in allIngredients" :key="index">
       <IngredientListItem
-        v-for="(item, index) in ingredientsList"
-        :key="index"
+        v-if="ingredientsList.includes(item)"
         class="mb-2 h-full"
         :text="item"
         :showMetrics="false"
-        :isChecked="ingredients.includes(item)"
+        :isChecked="ingredientsList.includes(item)"
       />
     </ul>
   </div>
--- src/views/ShoppingListView.vue
+++ src/views/ShoppingListView.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-import type { Ingredient } from '@/types/recipes'
 import { useMealPlanStore } from '@/stores/mealPlanStore'
 import MyIngriedientsList from '@/components/MyIngriedientsList.vue'
 import IngredientListItem from '@/components/IngredientListItem.vue'
 
 const mealPlanStore = useMealPlanStore()
-const allIngredients = computed<Ingredient[]>(() => mealPlanStore.extractedIngredients)
+const ingredientsList = computed<string[]>(() => {
+  const list = myIngredients.value.split(',')
+  return list.map((item) => item.trim().toLowerCase())
+})
+const allIngredients = computed<string[]>(() => [
+  ...mealPlanStore.extractedIngredients
+    .filter((item) => !ingredientsList.value.includes(item.name))
+    .map((item) => item.name)
+])
 const myIngredients = computed<string>(() => mealPlanStore.ingredients)
 </script>
 
@@ -21,14 +28,14 @@ const myIngredients = computed<string>(() => mealPlanStore.ingredients)
         <MyIngriedientsList />
       </div>
       <div class="col-span-3">
-        <h2 class="h3 mb-6">All ingredients</h2>
+        <h2 class="h3 mb-6">All your need for the recipes</h2>
         <ul class="grid grid-cols-4 gap-2">
           <IngredientListItem
-            v-for="item in allIngredients"
-            :key="item.id"
+            v-for="(item, index) in allIngredients"
+            :key="index"
             class="mb-2 h-full"
-            :text="item.name"
-            :isChecked="myIngredients.includes(item.name)"
+            :text="item"
+            :isChecked="ingredientsList.includes(item)"
             :simple="true"
             :showMetrics="false"
           />
